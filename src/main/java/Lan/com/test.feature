015945d5256ea3dce3ade765783d014b5d9ebce1
jguzmann <jguzman.cl@gmail.com>
2021-04-06 19:25:10 -0400

    Incorporando Cucumber

---
+++ src/main/java/Lan/com/test.feature
@@ -0,0 +1,10 @@
+Feature: Busco un vuelo ida y vuelta
+
+  yo
+  como cliente
+  quiero
+
+  Scenario: ingreso a la paguina o url "wwww.lan.com/cl"
+    And selecciono un vuelo de ida
+    And selecciono un vuelo de regreso
+    Then obtengo una buena tarifa
---
+++ src/main/java/test.java
@@ -0,0 +1,24 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class test {
+
+    @Given("selecciono un vuelo de ida")
+    public void selecciono_un_vuelo_de_ida() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("selecciono un vuelo de regreso")
+    public void selecciono_un_vuelo_de_regreso() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("obtengo una buena tarifa")
+    public void obtengo_una_buena_tarifa() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
