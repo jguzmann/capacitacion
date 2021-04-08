31edbf199cd1c6db02b5c28f34e501192c848f2f
jguzmann <jguzman.cl@gmail.com>
2021-04-07 21:31:41 -0400

    creacion prueba unitaria

---
+++ src/main/java/PruebaUnitaria.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class PruebaUnitaria {
+    String url = "https://www.latamairlines.com/cl/es";
+
+    WebDriver driver;
+    //System.setProperty ("webdriver.chrome.driver","c:\\chromedriver.exe");
+
+    driver = new ChromeDriver();
+
+    driver.get('url');
+
+
+
+
+
+
+
+
+}
+
+
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -5,20 +5,17 @@ public class test {
 
     @Given("selecciono un vuelo de ida")
     public void selecciono_un_vuelo_de_ida() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @Given("selecciono un vuelo de regreso")
     public void selecciono_un_vuelo_de_regreso() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @Then("obtengo una buena tarifa")
     public void obtengo_una_buena_tarifa() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
 }
